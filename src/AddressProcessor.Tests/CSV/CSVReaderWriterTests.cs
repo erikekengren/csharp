5b6526bf00f1ef3333ffcd6f95096cf72e803d3f
Erik Ekengren <erik.ekengren@gmail.com>
2016-09-21 22:53:56 +0100

    Initial commit

---
+++ src/AddressProcessor.Tests/CSV/CSVReaderWriterTests.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AddressProcessing.CSV;
+using System.IO;
+
+namespace Csv.Tests
+{
+	[TestFixture]
+	public class CSVReaderWriterTests
+	{
+		private List<string> _tempFiles = new List<string>();
+
+		[TestFixtureTearDown]
+		public void TestCleanup()
+		{
+			foreach (string tempFile in _tempFiles)
+			{
+				try
+				{
+					if (File.Exists(tempFile))
+						File.Delete(tempFile);
+				}
+				catch { }
+			}
+		}
+
+
+		#region Open
+
+		[Test]
+		[Category("Pass")]
+		public void Should_throw_on_invalid_open_filemode()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+			
+			Assert.Throws<ArgumentException>(() => { csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write | CSVReaderWriter.Mode.Read); });
+		}
+
+		[Test]
+		[Category("Pass")]
+		public void Should_create_file_on_open_for_write()
+		{
+			string fileName = GetTempFilePath();
+
+
+			Assert.True(!File.Exists(fileName));
+
+
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+
+			Assert.True(File.Exists(fileName));
+		}
+
+		#endregion
+
+		#region Write
+
+		[Test]
+		[Category("Pass")]
+		public void Should_write_columns()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+			csvReaderWriter.Write("COL 1", "column 2", "Test");
+
+			csvReaderWriter.Close();
+
+			string contents = string.Empty;
+
+			using (FileStream inputStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				using (StreamReader reader = new StreamReader(inputStream))
+				{
+					contents = reader.ReadToEnd();
+				}
+			}
+
+			Assert.IsNotNullOrEmpty(contents);
+
+			//strip newline
+			contents = contents.Replace(Environment.NewLine, string.Empty);
+
+
+			string[] columnsFromFile = contents.Split('\t');
+
+			Assert.AreEqual(3, columnsFromFile.Length);
+
+
+			Assert.AreEqual("COL 1", columnsFromFile[0]);
+
+			Assert.AreEqual("column 2", columnsFromFile[1]);
+
+			Assert.AreEqual("Test", columnsFromFile[2]);
+		}
+
+		[Test]
+		[Category("Pass")]
+		public void Should_throw_on_write_columns_with_no_items()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+			Assert.Throws<ArgumentException>(() => { csvReaderWriter.Write(new string[] { }); });
+		}
+
+		[Test]
+		[Category("Pass")]
+		public void Should_throw_on_write_null_columns()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+			Assert.Throws<ArgumentNullException>(() => { csvReaderWriter.Write(null); });
+		}
+
+		[Test]
+		[Category("Pass")]
+		public void Should_throw_on_write_columns_with_empty()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+			Assert.Throws<ArgumentException>(() => { csvReaderWriter.Write("COL 1", "", "Test"); });
+		}
+
+		#endregion
+
+		#region Read
+
+		[Test]
+		[Category("Pass")]
+		public void Should_fail_read_columns_from_file_without_out()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+
+			using (FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+			{
+				using (StreamWriter writer = new StreamWriter(outputStream))
+				{
+					writer.WriteLine("ITEM 1\titeM 2");
+				}
+			} 
+
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Read);
+
+			string column1 = string.Empty;
+			string column2 = string.Empty;
+
+
+			Assert.True(csvReaderWriter.Read(column1, column2));
+
+			Assert.AreEqual(string.Empty, column1);
+
+			Assert.AreEqual(string.Empty, column2);
+		}
+
+		[Test]
+		[Category("Pass")]
+		public void Should_read_columns_from_file()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+
+			using (FileStream outputStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+			{
+				using (StreamWriter writer = new StreamWriter(outputStream))
+				{
+					writer.WriteLine("ITEM 1\titeM 2");
+				}
+			}
+
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Read);
+
+			string column1 = string.Empty;
+			string column2 = string.Empty;
+
+
+			Assert.True(csvReaderWriter.Read(out column1, out column2));
+
+			Assert.AreEqual("ITEM 1", column1);
+
+			Assert.AreEqual("iteM 2", column2);
+		}
+
+		#endregion
+
+		#region Performance
+
+		[Test]
+		[Category("Performance")]
+		public void Should_write_columns_faster_with_updates()
+		{
+			CSVReaderWriter csvReaderWriter = new CSVReaderWriter();
+
+			string fileName = GetTempFilePath();
+
+			csvReaderWriter.Open(fileName, CSVReaderWriter.Mode.Write);
+
+			DateTime startTime = DateTime.Now;
+			for (int i = 0; i < 10000; i++)
+			{
+				csvReaderWriter.Write("COL 1", "column 2", "Test");
+			}
+			DateTime endTime = DateTime.Now;
+
+			csvReaderWriter.Close();
+
+
+			TimeSpan updatedDuration = endTime.Subtract(startTime);
+
+
+
+			CSVReaderWriterForAnnotation oldCsvReaderWriter = new CSVReaderWriterForAnnotation();
+
+			fileName = GetTempFilePath();
+
+			oldCsvReaderWriter.Open(fileName, CSVReaderWriterForAnnotation.Mode.Write);
+
+			startTime = DateTime.Now;
+			for (int i = 0; i < 100000; i++)
+			{
+				oldCsvReaderWriter.Write("COL 1", "column 2", "Test");
+			}
+			endTime = DateTime.Now;
+
+			oldCsvReaderWriter.Close();
+
+
+			TimeSpan oldDuration = endTime.Subtract(startTime);
+
+
+			string newTime = updatedDuration.ToString("c");
+			string oldTime = oldDuration.ToString("c");
+
+			System.Diagnostics.Debug.Write(newTime);
+			System.Diagnostics.Debug.Write(oldTime);
+
+			Assert.Less(updatedDuration.Ticks, oldDuration.Ticks);
+		}
+
+		#endregion
+
+		#region Private Medthods
+
+		private string GetTempFilePath()
+		{
+			string fileName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+			_tempFiles.Add(fileName);
+
+			return fileName;
+		}
+
+		#endregion
+	}
+}
---
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AddressProcessing.CSV
+{
+	/*
+		2) Refactor this class into clean, elegant, rock-solid & well performing code, without over-engineering.
+		   Assume this code is in production and backwards compatibility must be maintained.
+	*/
+
+	public class CSVReaderWriter : IDisposable
+	{
+		#region Variables
+
+		private StreamReader _readerStream = null;
+		private StreamWriter _writerStream = null;
+
+		private const int FIRST_COLUMN = 0;
+		private const int SECOND_COLUMN = 1;
+
+		private const char SEPARATOR = '\t';
+
+		#endregion
+
+		#region Enums
+
+		public enum Mode { Read = 1, Write = 2 };
+
+		#endregion
+
+		#region Destructor
+
+		~CSVReaderWriter()
+		{
+			Close();
+		}
+
+		#endregion
+
+		#region Open and Close
+
+		public void Open(string fileName, Mode mode)
+		{
+			if (string.IsNullOrWhiteSpace(fileName))
+				throw new ArgumentException("Parameter fileName cannot be null or empty");
+
+			if (mode == Mode.Read)
+			{
+				_readerStream = File.OpenText(fileName);
+			}
+			else if (mode == Mode.Write)
+			{
+				FileInfo fileInfo = new FileInfo(fileName);
+				_writerStream = fileInfo.CreateText();
+			}
+			else
+			{
+				throw new ArgumentException("Unknown file mode for " + fileName);
+			}
+		}
+
+		public void Close()
+		{
+			if (_writerStream != null)
+			{
+				_writerStream.Close();
+			}
+
+			if (_readerStream != null)
+			{
+				_readerStream.Close();
+			}
+		}
+
+		#endregion
+
+		#region Write
+
+		public void Write(params string[] columns)
+		{
+			if (columns == null)
+				throw new ArgumentNullException("Parameter columns cannot be null");
+
+			if (columns.Length == 0)
+				throw new ArgumentException("Parameter array columns contains no items");
+
+			StringBuilder outPut = new StringBuilder();
+
+			// No need to calculate this each iteration
+			int lastItem = columns.Length - 1;
+
+			for (int i = 0; i < columns.Length; i++)
+			{
+				string column = columns[i];
+
+				if (string.IsNullOrWhiteSpace(column))
+					throw new ArgumentException("Parameter columns contains empty items");
+
+				outPut.Append(column);
+
+				if (lastItem != i)
+				{
+					outPut.Append(SEPARATOR);
+				}
+			}
+
+			WriteLine(outPut.ToString());
+		}
+		
+		#endregion
+
+		#region Read
+
+		// Does nothing except advancing the reader to the next line and checking that there are columns
+		public bool Read(string column1, string column2)
+		{
+			string line;
+			string[] columns;
+
+			line = ReadLine();
+			columns = line.Split(SEPARATOR);
+
+			return (columns.Length != 0);
+		}
+
+		public bool Read(out string column1, out string column2)
+		{
+			if (_readerStream.EndOfStream)
+			{
+				column1 = null;
+				column2 = null;
+
+				return false;
+			}
+
+			string line;
+
+			line = ReadLine();
+
+			if (string.IsNullOrWhiteSpace(line))
+			{
+				column1 = null;
+				column2 = null;
+
+				return false;
+			}
+
+			// No point creating the array until the line has been checked
+			string[] columns;
+
+			columns = line.Split(SEPARATOR);
+
+			if (columns.Length < 2)
+			{
+				column1 = null;
+				column2 = null;
+
+				return false;
+			}
+			else
+			{
+				column1 = columns[FIRST_COLUMN];
+				column2 = columns[SECOND_COLUMN];
+
+				return true;
+			}
+		}
+		
+		#endregion
+
+		#region Private Methods
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void WriteLine(string line)
+		{
+			_writerStream.WriteLine(line);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private string ReadLine()
+		{
+			return _readerStream.ReadLine();
+		}
+
+		#endregion
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(this);
+
+			Close();
+		}
+		
+		#endregion
+	}
+}
